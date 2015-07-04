ce3aecd9a81741a585da8b2cc6d6cf3dc530382b
Tom Sabin <tom.sabin@unboxedconsulting.com>
2015-07-04 07:03:45 +0100

    Animations

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,13 @@
 //= require_tree .
+
+wow = new WOW({
+  boxClass: 'revealAnimated',
+  animateClass: 'animated',
+  offset: 160,
+  mobile: false,
+  live: true
+});
+
+wow.init();
+
+// mobile false does not work
