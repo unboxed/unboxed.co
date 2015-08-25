88571a62c82c73d6729488c6f2324d2ea63e5b4c
Tom Sabin <tom.sabin@unboxedconsulting.com>
2015-08-25 10:54:15 +0100

    Add blog layout

--- config.rb
+++ config.rb
@@ -7,6 +7,7 @@ set :markdown_engine, :redcarpet
 set :markdown, fenced_code_blocks: true, autolink: true, smartypants: true
 
 activate :blog do |blog|
+  blog.layout = 'blog'
   blog.prefix = 'blog'
   blog.permalink = "{title}.html"
 end
