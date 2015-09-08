044c84acee226b6f35629baec4400dea70720a0b
Tom Sabin <tom.sabin@unboxedconsulting.com>
2015-09-08 15:44:57 +0100

    Implement related blog articles weighting algorithm
    - Highest scoring articles will be same author + same/similar tags.
    - Second best scoring will be same/similar tags

--- middleman-blog-similar/algorithm/related_blog_articles.rb
+++ middleman-blog-similar/algorithm/related_blog_articles.rb
@@ -1,7 +1,7 @@
 require './lib/related_blog_articles'
 
 class Middleman::Blog::Similar::Algorithm::RelatedBlogArticles < ::Middleman::Blog::Similar::Algorithm
-  def similar_articles
-    ::RelatedBlogArticles.sort(article, articles)
+  def distance(other)
+    -::RelatedBlogArticles.new(article.data, other.data).weight
   end
 end
