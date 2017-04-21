c756cf4d6b00bac1826069c9bf31e302964ee95a
Murray Steele <muz@h-lame.com>
2017-04-21 12:29:20 +0100

    Ignore case when fetching articles by an author
    Neil's name in people.yml is "Neil Van Beinum", but in some of the blog posts he's
    written it's "Neil van Beinum" and these weren't being picked up.

--- spec/helpers_spec.rb
+++ spec/helpers_spec.rb
@@ -27,5 +27,27 @@ describe Helpers do
       expect(atom_id(article)).to eq "tag:unboxed.co,2014-10-29:i-like-hats"
     end
   end
+
+  describe 'articles_by_author' do
+    let(:article_1) { double("BlogArtcile", data: double('data', authors: ['Brian de la Smith'])) }
+    let(:article_2) { double("BlogArtcile", data: double('data', authors: ['Brian DE LA Smith', 'Simone Grant'])) }
+    let(:article_3) { double("BlogArtcile", data: double('data', authors: ['Aisling Noble'])) }
+    let(:blog) { double('blog', articles: [article_1, article_2, article_3]) }
+    before do
+      allow(self).to receive(:blog).with(:blog).and_return(blog)
+    end
+
+    it 'includes articles written by the supplied author if they are the sole author' do
+      expect(articles_by_author('Aisling Noble')).to eq [article_3]
+    end
+
+    it 'includes articles written by the supplied author if they are one of several authors' do
+      expect(articles_by_author('Simone Grant')).to eq [article_2]
+    end
+
+    it 'includes articles written by the supplied author even if the case is wrong' do
+      expect(articles_by_author('Brian de la Smith')).to eq [article_1, article_2]
+    end
+  end
 end
 
