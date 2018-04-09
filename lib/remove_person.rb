345572fc0a6667bf18eadc7d692bbefba6dcd97a
Tom Sabin <tom.sabin@unboxed.co>
2018-04-09 15:56:10 +0100

    Refactor removal of person from posts and pages
    Remove dependency on `ag` command and use system's `grep` command to
    find files instead. Also target only product stories to remove links to
    the page page. Currently, we do not remove any other links (for example,
    blog posts).

--- lib/remove_person.rb
+++ lib/remove_person.rb
@@ -3,8 +3,6 @@ require 'yaml'
 
 puts "Enter name of person to remove:"
 name = gets.strip!
-short_name = name.gsub(" ", "-").downcase
-first_name = name.split(" ")[0]
 
 ##########################################
 # Remove person from people.yml          #
@@ -21,43 +19,16 @@ else
   puts "Removed #{name} from 'people.yml'."
 end
 
-#############################################################
-# Search through posts and remove links to people/#{name}   #
-#############################################################
+###########################################################################
+# Search through product stories and remove links to people/#{short_name} #
+###########################################################################
 
-puts "Searching for people page links..."
-people_page_link_regex = Regexp.new ".*\(\D*\/people##{short_name}\).*"
-ag_people_page_link_regex = "'.*\(\D*\/people##{short_name}\).*'"
-dead_link_count = 0
+short_name = name.gsub(" ", "-").downcase
+source_files = %x(grep -rwl ./source/product-stories -e 'people##{short_name}').split
 
-source_files = `ag #{ag_people_page_link_regex} source/ -l`.split("\n")
-
-source_files.each do |source_file|
-  source_file_path = "#{Dir.pwd}/#{source_file}"
-  tmp_src_file_path = "#{source_file_path}.tmp"
-
-  File.open(tmp_src_file_path, "w") do |output|
-    File.open(source_file, "r") do |input|
-      input.each_line do |line|
-        if people_page_link_regex.match(line)
-          partitioned_line = line.partition(/\[#{Regexp.quote first_name}\D*?\]/)
-          remainder_of_line = partitioned_line[2].partition(/\(.*\/people##{Regexp.quote(short_name)}\)/)
-          clean_line = "#{partitioned_line[0]}#{partitioned_line[1][1..-2]}#{remainder_of_line[2]}"
-
-          output.write(clean_line)
-          dead_link_count += 1
-        else
-          output.write(line)
-        end
-      end
-    end
-  end
-
-  # Delete old file and write in new one
-  File.delete(source_file_path)
-  File.rename(tmp_src_file_path, source_file_path)
+if source_files.any?
+  puts "Attempting to clean up links in: \n- #{source_files.join("\n- ")}."
+  %x(sed -i '' -e '/^.*people##{short_name}.*$/d' #{source_files.join(' ')})
+else
+  puts "Could not find #{short_name} links in product story pages."
 end
-
-puts "Found and repaired #{dead_link_count} dead links."
-puts "Exiting..."
-exit
